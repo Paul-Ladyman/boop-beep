05ad3305591ebbc7623ecd2509420ae4b44a32e2
Paul-Ladyman <ladyman.paul@gmail.com>
2018-04-29 13:20:13 +0100

    Use timer0 to produce PWM base signal. Use fewer duty cycle steps to maintain higher frequency.

--- main.c
+++ main.c
@@ -13,9 +13,9 @@
 #define LED1 RC4
 #define LED2 RC5
 
-int thresh0 = 75;
-int thresh1 = 50;
-int thresh2 = 200;
+int pwmThresh0 = 90;
+int pwmThresh1 = 99;
+int pwmThresh2 = 85;
 
 void initMain() {
     // Set ports as digital I/O, not analogue input
@@ -26,43 +26,62 @@ void initMain() {
      * Here we set all pins to be outputs (0)
      */
     TRISC = 0;
+
+    // Set to timer mode (internal clock source)
+    OPTION_REGbits.T0CS = 0;
+
+    // Assign Prescaler to TIMER0 instead of WatchDogTimer 
+    OPTION_REGbits.PSA = 0;
+    // Set Prescaler to 16
+    OPTION_REGbits.PS = 0b000;
+
+    TMR0 = 180;
+
+    // enable Timer0 interrupt
+    INTCONbits.T0IE = 1;
+    // Enable global interrupts
+    INTCONbits.GIE = 1;
 }
 
 void main(void) {
     initMain();
 
-    int count = 0;
-    int rising = 1;
+    while (1);
+}
 
-    while (1) {
-        if (rising == 1) {
-            count++;
-        } else {
-            count--;
-        }
+void interrupt ISR(void) {
+    static int pwmBaseCount = 0;
+    // Reset timer flag
+    INTCONbits.T0IF = 0;
+    TMR0 = 180;
 
-        if (count == 300) {
-            rising = 0;
-        } else if (count == 0) {
-            rising = 1;
-        }
+    /* 
+     * Given 8MHz osc, 1:1 prescaler, 8-bit Timer (256) with 180 offset,
+     * 100 counts to complete pwmBase signalcount
+     * freq = 8MHz / 4 / 1 / 76 / 100 = 263Hz
+     */
+    if (pwmBaseCount < 100) {
+        pwmBaseCount++;
+    } else {
+        pwmBaseCount = 0;
+    }
 
-        if (count > thresh0) {
-            LED0 = 1;
-        } else {
-            LED0 = 0;
-        }
+    // LED turns on when pin is low
+    if (pwmBaseCount > pwmThresh0) {
+        LED0 = 0;
+    } else {
+        LED0 = 1;
+    }
 
-        if (count > thresh1) {
-            LED1 = 1;
-        } else {
-            LED1 = 0;
-        }
+    if (pwmBaseCount > pwmThresh1) {
+        LED1 = 0;
+    } else {
+        LED1 = 1;
+    }
 
-        if (count > thresh2) {
-            LED2 = 1;
-        } else {
-            LED2 = 0;
-        }
+    if (pwmBaseCount > pwmThresh2) {
+        LED2 = 0;
+    } else {
+        LED2 = 1;
     }
 }
